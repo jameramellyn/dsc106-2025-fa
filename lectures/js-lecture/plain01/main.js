59c5a7489d958d3aa04de07c4e9e6c0f8779576c
jameramellyn <107943103+jameramellyn@users.noreply.github.com>
2026-05-19 20:55:12 -0700

    dataframes

--- lectures/js-lecture/plain01/main.js
+++
@@ -1,6 +0,0 @@
-// button is null, why??
-const button = document.querySelector('#submit');
-
-button.addEventListener('click', (event) => {
-  console.log(event);
-});
--- lectures/js-lecture/plain02/main.js
+++
@@ -1,8 +0,0 @@
-const celsiusTag = document.querySelector('#celsius');
-const result = document.querySelector('#result');
-
-celsiusTag.addEventListener('input', (event) => {
-  const celsius = celsiusTag.value;
-  const fah = (celsius * 9) / 5 + 32;
-  result.innerText = `${celsius} degrees Celsius is ${fah} degrees Fahrenheit.`;
-});
